8e04b3e20e92d3f89d28963d76b08619e6f2f414
USER <user@USERs-MacBook-Pro.local>
2020-06-26 04:20:22 -0400

    cleaned up code

--- src/components/Wordbank.js
+++ src/components/Wordbank.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React from "react";
 
-
-const Wordbank = props => {
-
+const Wordbank = (props) => {
   let count = 0;
   let completed = true;
-  while (count < props.wordlist.length) {  //Check if the wordsearch has been completed
+  while (count < props.wordlist.length) {
+    //Check if the wordsearch has been completed
     if (!props.wordlist[count][3]) {
       completed = false;
       break;
@@ -14,18 +13,21 @@ const Wordbank = props => {
   }
 
   return (
-    <div className = "wordBankContainer">
-        <h2> Word Bank</h2>
-        {props.wordlist.map((item) => (
-          <p className = {(item[3] ? "found" : "")}> {item[0]} </p>
-        ))}
-        {completed 
-        && 
-        <button onClick = {() =>{window.location.reload()}}> Play Again</button>
-        }
+    <div className='wordBankContainer'>
+      <h2> Word Bank</h2>
+      {props.wordlist.map((item) => (
+        <p className={item[3] ? "found" : ""}> {item[0]} </p>
+      ))}
+      {completed && (
+        <button
+          onClick={() => {
+            window.location.reload();
+          }}>
+          Play Again
+        </button>
+      )}
     </div>
-    );
-  }
-
+  );
+};
 
 export default Wordbank;
